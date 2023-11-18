54fc5882dfb1c07e9cb7be33064564ebf7b8f8df
Sai Teja <saitejapubglite@gmail.com>
2023-11-18 16:59:34 +0530

    update

--- score.js
+++ score.js
@@ -1,1 +1,14 @@
 // Iteration 8: Making scoreboard functional
+
+var urlParams = new URLSearchParams(window.location.search)
+console.log(urlParams)
+var score = urlParams.get("score")
+
+var scoreboard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreboard.innerHTML = score;
+playAgainButton.onclick = (evt) => {
+    location.href = "./game.html";
+};
